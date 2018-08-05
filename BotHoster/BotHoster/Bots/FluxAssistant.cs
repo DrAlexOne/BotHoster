3701d562ec978d2ae0eacbfe937504ba771b238f
DrAlexOne <DrAlexOne@users.noreply.github.com>
2018-08-05 20:13:48 +0300

    Updated

--- BotHoster/BotHoster/Bots/FluxAssistant.cs
+++ BotHoster/BotHoster/Bots/FluxAssistant.cs
@@ -35,7 +35,6 @@ namespace BotHoster
                 var Bot = new Telegram.Bot.TelegramBotClient(key);
 
                 int offset = 0;
-                bool MessageChecked = false;
                 while (true)
                 {
                     var updates = await Bot.GetUpdatesAsync(offset);
@@ -48,73 +47,73 @@ namespace BotHoster
                             if (message.Text == "/start" || message.Text == "/start@" + AppSettings.FAName)
                             {
                                 await Bot.SendTextMessageAsync(message.Chat.Id, "Good day! I'm Flux Assistant. I am always ready to help you. Right now, I am in Alpha Version. My developer is @AlexBesida", replyMarkup: new InlineKeyboardMarkup(InlineKeyboardButton.WithUrl("Alex Besida", "https://t.me/AlexBesida")));
-
-
-                                MessageChecked = true;
                             }
 
-                            if (message.Text == "/developer" || message.Text == "/developer@" + AppSettings.FAName)
+                            else if (message.Text == "/developer" || message.Text == "/developer@" + AppSettings.FAName)
                             {
 
                                 await Bot.SendTextMessageAsync(message.Chat.Id, "My developer is Alex Besida.", replyMarkup: new InlineKeyboardMarkup(InlineKeyboardButton.WithUrl("Alex Besida", "https://t.me/AlexBesida")));
-                                MessageChecked = true;
 
                             }
 
-                            if (message.Text == "/feedback" || message.Text == "/@" + AppSettings.FAName)
+                            else if (message.Text == "/feedback" || message.Text == "/@" + AppSettings.FAName)
+                            {
+                                await Bot.SendTextMessageAsync(message.Chat.Id, comingsoon);
+                            }
+
+                            else if (message.Text == "/help" || message.Text == "/help@" + AppSettings.FAName)
                             {
                                 await Bot.SendTextMessageAsync(message.Chat.Id, comingsoon);
 
-                                MessageChecked = true;
 
                             }
 
-                            if (message.Text == "/help" || message.Text == "/help@" + AppSettings.FAName)
+                            else if (message.Text == "/settings" || message.Text == "/settings@" + AppSettings.FAName)
                             {
                                 await Bot.SendTextMessageAsync(message.Chat.Id, comingsoon);
 
-                                MessageChecked = true;
 
                             }
 
-                            if (message.Text == "/settings" || message.Text == "/settings@" + AppSettings.FAName)
-                            {
-                                await Bot.SendTextMessageAsync(message.Chat.Id, comingsoon);
-
-                                MessageChecked = true;
-
-                            }
-
-                            if (message.Text == "/myid" || message.Text == "/myid@" + AppSettings.FAName)
+                            else if (message.Text == "/myid" || message.Text == "/myid@" + AppSettings.FAName)
                             {
                                 await Bot.SendTextMessageAsync(message.Chat.Id, "Your ID is: " + message.From.Id + ".");
-
-                                MessageChecked = true;
-
                             }
 
-                            if (message.Text.Contains("Hello") || message.Text.Contains("Hey") || message.Text.Contains("Hi") || message.Text.Contains("Hola") || message.Text.Contains("Hoy") || message.Text.Contains("Good day"))
+                            else if (message.Text.Contains("Hello") || message.Text.Contains("Hey") || message.Text.Contains("Hi") || message.Text.Contains("Hola") || message.Text.Contains("Hoy") || message.Text.Contains("Good day"))
                             {
                                 await Bot.SendTextMessageAsync(message.Chat.Id, "Hello!");
 
-                                MessageChecked = true;
 
                             }
 
-                            if (message.Text.Contains("XD") || message.Text.Contains("xd") || message.Text.Contains("Xd") || message.Text.Contains("xD"))
+                            else if (message.Text.Contains("XD") || message.Text.Contains("xd") || message.Text.Contains("Xd") || message.Text.Contains("xD"))
                             {
                                 await Bot.SendTextMessageAsync(message.Chat.Id, "A little laugh will not prevent you", replyToMessageId: message.MessageId);
 
-                                MessageChecked = true;
 
                             }
 
-                            if (message.Text.Contains("Okey") || message.Text.Contains("Ok") || message.Text.Contains("OK"))
+                            else if (message.Text.Contains("Okey") || message.Text.Contains("Ok") || message.Text.Contains("OK"))
                             {
                                 await Bot.SendTextMessageAsync(message.Chat.Id, "👌 🤙", replyToMessageId: message.MessageId);
+                            }
+                            else if (message.Text.Contains("How are you") || message.Text.Contains("Whats up") || message.Text.Contains("What's up"))
+                            {
+                                await Bot.SendTextMessageAsync(message.Chat.Id, "All fine, thanks. And you?", replyToMessageId: message.MessageId);
+                            }
 
-                                MessageChecked = true;
+                            else if (message.Text.Contains("good") || message.Text.Contains("Good") || message.Text.Contains("Nice") || message.Text.Contains("Fine"))
+                            {
+                                await Bot.SendTextMessageAsync(message.Chat.Id, "Okey 👌");
+                            }
 
+
+
+
+                            else
+                            {
+                                await Bot.SendTextMessageAsync(message.Chat.Id, "Sorry, but I did not understand.");
                             }
                         }
 
